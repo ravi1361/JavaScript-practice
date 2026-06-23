3500f1695450e250e67b9a173e964b899f416b78
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-23 14:18:37 +0530

    functions in js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-//Types in js
+//functions
 
-//reference -- [], (), {}
+function oatsBanaoAndKhaoo(){
+  console.log("oats khareedo");
+  console.log("pateela chadao gas par");
+  console.log("paani daalo and gas on karo");
+  console.log("oats daalo");
+  console.log("mix it and boil");
+  console.log("eat");
+}
 
-var a = [12,13];
-var b = a;
+oatsBanaoAndKhaoo();
 
-b.pop();
-
-console.log("hello");
-
-
-
-
-
-
-
+oatsBanaoAndKhaoo();
