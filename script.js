5eb24e0f36029ac69a4b4ef333f0e5125c0ec669
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-26 18:48:35 +0530

    filled object

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // objects
 
-//blanked object
-var a = {};
+//filled object
+var a = {
+  age : 24,
+  name: "harsh",
+  email: "email@male.com",
+  linkedin: "harsh",
+  contact: 98758712598,
+  kuchbolo: function(){}
+}
