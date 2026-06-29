9427b3b88b07ac5c928ee88c0c86d965d5e1034b
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-29 17:36:06 +0530

    forEach loop

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// truthy falsy values
+// forEach loop
 
-// falsy values  -- 0, false, undefined, null, NaN, document.all
-// truthy values -- the values which are not belonging to falsy values
+// forEach loop is for array
 
-// falsy values 
-if(0){
-  console.log("hey");
-}
-else{
-  console.log("hello");
+var a = [1,2,23,34,4,45];
+
+
+// the copy of array comes here , not the original array
+a.forEach(function(val){
+  console.log(val+2); // it added in temporary copy , it won't reflect in real array
   
-}
-
+})
