0add410a834e4c8ced58f584cdb30eb1b968ac4f
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-29 17:32:01 +0530

    falsy values

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
 // falsy values  -- 0, false, undefined, null, NaN, document.all
 // truthy values -- the values which are not belonging to falsy values
 
-// truthy values
-if("harsh"){
+// falsy values 
+if(0){
   console.log("hey");
-  
 }
 else{
   console.log("hello");
+  
 }
+
