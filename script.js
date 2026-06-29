57b38b7a54667d430f3b1f54e8a8a7b93f555487
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-29 17:26:17 +0530

    how to copy objects

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// how to copy reference values
+// how to copy objects
 
-var a = [1,2,3,4,5];
-var b = [...a];   // spread operator
-
-b.pop();
-
-
-
-// b will have [1,2,3,4] 
-// a will have [1,2,3,4,5] , because b copied the values 
+var obj = {name: "hasrh"};
+var copyobj = {...obj};
