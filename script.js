ce6252898f7240b4049fc05578b84b7d40e81e14
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-30 16:22:04 +0530

    for in loop

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// forEach loop
+// for in 
 
-// forEach loop is for array
+var obj = {
+  name: "hasrh",
+  age: 24 ,
+  city: "bhopal"
+}
 
-var a = [1,2,23,34,4,45];
-
-
-// the copy of array comes here , not the original array
-a.forEach(function(val){
-  console.log(val+2); // it added in temporary copy , it won't reflect in real array
-  
-})
+for(var key in obj){
+   console.log(key, obj[key]);
+}
