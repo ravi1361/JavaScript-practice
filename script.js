aef1c3940af4dc8989562f62e19745b3c901dbca
Ravi Hiremath <hiremathravi333@gmail.com>
2026-07-02 18:22:53 +0530

    iife, using getter setter

--- script.js
+++ script.js
@@ -8,9 +8,17 @@ var ans = (function(){
   return {
     getter: function(){
       console.log(privateval);
+    },
+    setter: function(){   // setter value maangta hai
+      privateval = val ;   // jo bhi value bhejoge wo set ho jayega
     }
   }
 }) ()
 
 
+// output will be like this
+// > ans.setter(24)
+// <. undefined
 
+// ans.getter()
+// 24
