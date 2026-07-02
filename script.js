d954cfcc657c833a17e682ad26947d2bd6fd35e4
Ravi Hiremath <hiremathravi333@gmail.com>
2026-07-02 18:20:21 +0530

    iife

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-// constructor function
-
-// normal function jisme "this" ka istemaal ho and aap function ko call karte waqt "new" keyword ka use karre
-
-// jabke aaple paas aisa koi bhi mauka ho ke aapko ek jaise properties waale bahutt saare elements banane hai us waqt aap constructor function use kar sakte ho
+// iife -- immediately invoked function expression 
 
 
-function circularButtonBanao(color){
-  this.radius = 2 ;
-  this.color = color ;
-  this.icon =  false;
-  this.pressable = true;
-}
+ //iife
+
+var ans = (function(){
+  var privateval = 12 ;
+  return {
+    getter: function(){
+      console.log(privateval);
+    }
+  }
+}) ()
 
 
-var redbtn = new circularButtonBanao("red");
-var greenbtn = new circularButtonBanao("green");
+
