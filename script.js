6475944e4cc03feb3eae940481a54cbc940432d5
Ravi Hiremath <hiremathravi333@gmail.com>
2026-07-03 18:35:41 +0530

    passing argument while using call

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // call
-// this will point to object here
-function abcd(){
-  console.log(this);
+// we can send more than 2 arguments while calling function 
+function abcd(val1,val2,val3){
+  console.log(this,val1,val2,val3);
 }
 
 var obj = {age:24}
 
-abcd.call(obj)
+abcd.call(obj,1,2,3)
