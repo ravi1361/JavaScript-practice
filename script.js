41fc22e4d7405167b963b9fdc799e7cdbd93c30d
Ravi Hiremath <hiremathravi333@gmail.com>
2026-07-03 18:32:10 +0530

    call

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// "this" keyword in eventlistener
-// this keyword is equals to whatever written before addEventListener
-
-var button = document.querySelector("button");
-button.addEventListener("click", function(){
+// call
+// this will point to object here
+function abcd(){
   console.log(this);
-  this.style.color = "red" ;
-})
+}
+
+var obj = {age:24}
+
+abcd.call(obj)
