016f36dee00533447b9b048eed066ce8f277c9e8
Ravi Hiremath <hiremathravi333@gmail.com>
2026-07-03 18:37:06 +0530

    apply

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// call
-// we can send more than 2 arguments while calling function 
+// apply
+// apply will take only 2 arguments 
 function abcd(val1,val2,val3){
   console.log(this,val1,val2,val3);
 }
 
 var obj = {age:24}
 
-abcd.call(obj,1,2,3)
+abcd.apply(obj,[1,2,3])
