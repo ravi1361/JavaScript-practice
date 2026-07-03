e1d81c91ae7ba54ff7318df09893f83d860dbe44
Ravi Hiremath <hiremathravi333@gmail.com>
2026-07-03 18:28:25 +0530

    "this" keyword in eventListener

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-// "this" keyword
+// "this" keyword in eventlistener
+// this keyword is equals to whatever written before addEventListener
 
-// "this" values in:
-// 1) global ---- window
-// 2) function -- window
-// 3) method ---- object
-
-// 1) global scope
-// console.log(this) // gives window in console
-
-
-// 2) function scope
-//  function abcd(){
-//    console.log(this);
-// }
-// abcd();              // gives window in console
-
-// 3) method scope
-var obj = {
-  name: "harsh",
-  baatKaro: function(){     // its a method
-    console.log(this);
-    
-  }
-}
-
-obj.baatKaro();
-
-
-//in any method , "this" keyword always refers to parent object
+var button = document.querySelector("button");
+button.addEventListener("click", function(){
+  console.log(this);
+  this.style.color = "red" ;
+})
