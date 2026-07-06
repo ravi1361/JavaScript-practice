0ea18357e0960d8991415a7929a76841c6e2c943
Ravi Hiremath <hiremathravi333@gmail.com>
2026-07-06 17:32:55 +0530

    basic fat arrow function

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// callbacks
+// 1) basic fat arrow function
 
-// callback hamesha ek function hota hai , 
-// ye sirf tab chalta hai jab async code ka completion hojaata hai
-
-var ans = new Promise((res,rej) => {
- if(false){
-  return res();
- }else{
-  return rej();
- }
-})
-
-
-ans
-.then(function(){
-  console.log("resolve hogaya tha");
-})
-.catch(function(){
-  console.log("reject hua tha");
-  
-})
+var a = () => {};
