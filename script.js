c5242378bb26cbcc96f1998efdeb6f67192df6a4
Ravi Hiremath <hiremathravi333@gmail.com>
2026-07-09 20:15:06 +0530

    spread operator

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// default parameter
+// spread operator
 
-// parameter ko value de dena
-
-function abcd(a=0,b=0,c=0){
-  console.log(a,b,c);
-}
-
-abcd(1,2,3);
-abcd(4,5,6);
-abcd(1);
-
-// koi value nahi bheja to , parameter ke andar value jo set ki hogi ,wo value le lega , agar koi value set nahi ki parameter ke andar toh , wo undefined batadega
+var a = [1,2,3];
+var b = [...a];
