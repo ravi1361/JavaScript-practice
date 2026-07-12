5932366d43d6946abbcabc08e2b553d418f6728b
Ravi Hiremath <hiremathravi333@gmail.com>
2026-07-12 12:07:55 +0530

    redeclaration of variables

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// variables and declaration
 
- var a ; // declaration
- var a = 12 ; // declare and initialization
 
- // initialization means pehli value dena
+var a = 12 ;
+
+var a = 133;
+
+// we can redeclare the variable and it will not give any error , which is not good
+
+
+
+let b = 12 ;
+let b = 133;
+
+// if we redeclare the let , then it will throw an error
