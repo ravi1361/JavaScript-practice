e44fb5e132258f8c47bce45a5d6119e1f31a25ab
Ravi Hiremath <hiremathravi333@gmail.com>
2026-07-30 20:35:38 +0530

    rest parameter

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// default parameter
-function add(v1=0, v2=0){
-  console.log(v1+v2);
+// rest parameter --
+// jab arguments kai saare ho to humein utne hi parameter banane padenge ,
+//  issey bachne ke liye ,hum rest ka use karte hai
+function abcd(...val){
+  console.log(val);
 }
-add(1,2);
+abcd(1,2,3,4,5,6);
