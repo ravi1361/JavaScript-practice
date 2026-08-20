c6d5960c902527bf6f44279832a09221e77a6c15
Ravi Hiremath <hiremathravi333@gmail.com>
2026-08-20 21:50:37 +0530

    nesting and deep access

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-// accessing the object
+// nesting and deep access
 
-let obj = {
-  name:"harsh",
-  age:26,
-  khaana:"daal chawal"
-};
+const user = {
+  name:"Harsh",
+  address:{
+    city:"bhopal",
+    pin:462001,
+    location:{
+      lat:23.2,
+      lng:77.4
+    },
+  },
+}
 
-let aa = "name" ;
-obj[aa]
+
+// user.address.location.lng gives 77.4
