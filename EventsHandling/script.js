4df734ea4f898dd0c009f3fce11544b6a24dd26b
Ravi Hiremath <hiremathravi333@gmail.com>
2026-09-07 21:38:57 +0530

    dblClick

--- EventsHandling/script.js
+++ EventsHandling/script.js
@@ -1,5 +1,5 @@
 let h1 = document.querySelector("h1");
 
-h1.addEventListener("click",function(){
-  h1.style.color = "red" ;
+h1.addEventListener("dblclick",function(){
+  h1.style.color = "yellow" ;
 })
