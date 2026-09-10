8d509fae807d1a0590a701a78ed0baaf1083bf59
Ravi Hiremath <hiremathravi333@gmail.com>
2026-09-10 19:11:55 +0530

    Log typed character using input event data

--- EventsHandling/script.js
+++ EventsHandling/script.js
@@ -1,6 +1,6 @@
 let input = document.querySelector("input");
 
-input.addEventListener("input",function(){
-  console.log("typed");
+input.addEventListener("input",function(dets){
+  console.log(dets.data);
   
 })
