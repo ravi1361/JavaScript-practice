2023b49fd9af7073007a35d39225b046c1e69c4b
Ravi Hiremath <hiremathravi333@gmail.com>
2026-09-18 14:27:10 +0530

    Add mouseover and mouseout background color effects

--- EventsHandling/script.js
+++ EventsHandling/script.js
@@ -1,40 +1,9 @@
-let form = document.querySelector("form");
-let inputs = document.querySelectorAll("input");
-let main = document.querySelector("#main");
+let abcd = document.querySelector("#abcd");
 
-form.addEventListener("submit", function(dets){
-dets.preventDefault();
-
-let card = document.createElement("div");
-card.classList.add("card")
-
-let profile = document.createElement("div");
-profile.classList.add("profile");
-
-let img = document.createElement("img");
-img.setAttribute("src",inputs[0].value);
-
-let h3 = document.createElement("h3");
-h3.textContent = "Harsh";
-
-let h5 = document.createElement("h5");
-h5.textContent = "coder";
-
-let p = document.createElement("p");
-p.textContent = "good guy";
-
-profile.appendChild(img);
-card.appendChild(profile);
-
-card.appendChild(h3);
-card.appendChild(h5);
-card.appendChild(p);
-
-main.appendChild(card);
-
-inputs.forEach(function(inp){
-  if(inp.type !== "submit"){
-    inp.value = " ";
-  }
-})
+abcd.addEventListener("mouseover",function(){
+  abcd.style.backgroundColor = "yellow";
 });
+
+abcd.addEventListener("mouseout",function(){
+  abcd.style.backgroundColor = "red";
+})
