9b83fca4a5d72f8baeb6678c166aaf82525afdf3
Ravi Hiremath <hiremathravi333@gmail.com>
2026-09-19 18:56:52 +0530

    add mousemove event listener

--- EventsHandling/script.js
+++ EventsHandling/script.js
@@ -1,9 +1,4 @@
-let abcd = document.querySelector("#abcd");
-
-abcd.addEventListener("mouseover",function(){
-  abcd.style.backgroundColor = "yellow";
-});
-
-abcd.addEventListener("mouseout",function(){
-  abcd.style.backgroundColor = "red";
+window.addEventListener("mousemove", function(dets){
+  console.log(dets);
+  
 })
